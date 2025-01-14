74b733bf491e27c802b55b635a533e8ae8049f5b
ZAZiOs <85897688+ZAZiOs@users.noreply.github.com>
2025-01-14 18:10:18 +0300

    final_page_experimental

--- src/lib/card.svelte
+++ src/lib/card.svelte
@@ -1,12 +1,43 @@
 <script>
-    export let name, color, color2, congrats
+    export let name, color, color2, congrats, date
     if (!name) name = 'Имя неизвестного человека'
     if (!congrats) congrats = 'Пример для поздравления'
     if (!color) color = '#de831b'
+    if (!color2) color2 = '#de8585'
+    let randomY = Math.floor(Math.random() * 50)
+    let randomX = Math.ceil(Math.random() * 100) * (Math.round(Math.random()) ? 1 : -1)
+    let random_deg = Math.ceil(Math.random() * 10) * (Math.round(Math.random()) ? 1 : -1)
+    date = new Date(date)
 </script>
 
-
-<div class="dm-card-container" style="--gradient-color1: {color}; --gradient-color2: {color2}">
+<div class="dm-card-container" style="--gradient-color1: {color}; --gradient-color2: {color2}; top: {randomY}px; left: {randomX}px; --rotate: {random_deg}deg;">
+    <svg width="40" height="33" viewBox="0 0 33 33" fill="none" xmlns="http://www.w3.org/2000/svg" class="btn-img"
+    id='color-{color}'>
+    <g filter="url(#filter0_d_1_30)">
+        <circle cx="16.5" cy="12.5" r="12.5" fill="url(#paint0_linear_1_30-{color}-{color2})" />
+    </g>
+    <circle cx="11" cy="6" r="2" fill="white" fill-opacity="0.4" />
+    <circle cx="10" cy="5" r="1" fill="white" fill-opacity="0.4" />
+    <defs>
+        <filter id="filter0_d_1_30" x="0" y="0" width="33" height="40" filterUnits="userSpaceOnUse"
+            color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                result="hardAlpha" />
+            <feOffset dy="2" dx='1' />
+            <feGaussianBlur stdDeviation="2" />
+            <feComposite in2="hardAlpha" operator="out" />
+            <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.60 0" />
+            <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1_30" />
+            <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1_30" result="shape" />
+        </filter>
+        <linearGradient id="paint0_linear_1_30-{color}-{color2}" x1="12.3693" y1="4.18793e-07" x2="20.6307" y2="25"
+            gradientUnits="userSpaceOnUse">
+            <stop stop-color={color} />
+            <stop offset="1" stop-color={color2} />
+        </linearGradient>
+    </defs>
+    </svg>
     <div class="progress-bar-gradient main-gradient">
         <div class="progress rounded-bottom-0">
             <div class="progress-bar progress-bar-striped progress-bar-animated progress-gradient" role="progressbar" style="width: 100%;"></div>
@@ -15,20 +46,39 @@
     <div class="dm-card-bg main-gradient">
         <div class="dm-card">
             <div class="dm-card-body">
-                <h2 class="m-0">{name}</h2>
+                <h2 class="m-0 mb-2">{name}</h2>
                 <div class="overflow-scrolling">
                     {@html congrats.replaceAll('\n\n', '\n').replaceAll('\n', '<br>')}
                 </div>
+                <h6 class="text-dark-subtle opacity-50 mt-2 mx-2">{date.getDate()}.{date.getMonth() + 1}.{date.getFullYear()} {date.getHours()}:{date.getMinutes()}</h6>
             </div>
         </div>
     </div>
 </div>
 
 <style>
+    .btn-img {
+        position: relative;
+        top: 27px;
+    }
+    .dm-card-container:hover {
+        z-index: 2;
+        transition: all .2s;
+    }
     .dm-card-container {
+        transition: all .2s;
+        z-index: 0;
+        position: relative;
+        top: 0;
+        --rotate: 0deg;
+        transform: rotate(var(--rotate));
         --gradient-color1: #de831b;
         --gradient-color2: #de8585;
-        max-width: 500px;
+        width: 400px;
+        margin-left: 75px;
+        margin-right: 75px;
+        text-align: center;
+        margin-bottom: 50px;
     }
     .dm-card-bg {
         --radius: 7px;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,58 +1,30 @@
 <script>
     export let data
-    import Card from "$lib/card.svelte";
-    import AltCard from "$lib/alt-cards.svelte";
-    let congratulation = data.props.old_post.congrats || ''
-    let name = data.props.old_post.name || ''
-    let color = data.props.old_post.color1 || '#de831b'
-    let color2 = data.props.old_post.name.color2 || '#de381b'
+    import Card from '$lib/card.svelte'
+    let yp = data.old_post
+    let posts = data.posts
 </script>
 
-<div class="container text-center mt-5 d-flex flex-column">
-    <h1>Поздравление для Даниила Мысливца</h1>
-    <div class="alert alert-warning d-flex align-items-center" role="alert">
-        <i class="bi bi-exclamation-triangle-fill me-2"></i> 
-        Всё что вы введёте на данном сайте пройдёт модерацию и будет опубликовано публично!
-      </div>
-    <div class="main-box mb-3">
-        <form method="POST" action="/?/post">
-            <div class="form-floating mb-3">
-                <input bind:value={name} type="text" name="name" class={name.length <= 25 ? "form-control" : "form-control is-invalid"} id="nameInput" placeholder="ZAZiOs">
-                <label for="nameInput">Ваше имя <sup class="text-secondary">(или никнейм) [{name.length} / 25]</sup></label>
-            </div>
-            <div class="form-floating mb-3">
-                <textarea bind:value={congratulation} name="congrats" class={congratulation.length <= 1024 ? "form-control" : "form-control is-invalid"} placeholder="Оставьте ваше поздравление тут" id="congratInput" style="height: 100px"></textarea>
-                <label for="congratInput">Поздравление <sup class="text-secondary">[{congratulation.length} / 1024]</sup></label>
-            </div>
-            <div class="form-floating">
-                <div class="progress" id="colorbar" style="height: auto">
-                    <input type="color" class="form-control form-control-color w-100 progress-bar progress-bar-striped progress-bar-animated" 
-                    style="background-color: {color}; padding: calc(1rem + calc(var(--bs-border-width)* 2));border-top-right-radius: 0;border-bottom-right-radius: 0;" 
-                    bind:value={color} placeholder="Шкала цвета" name="color1">
-                    <input type="color" class="form-control form-control-color w-100 progress-bar progress-bar-striped progress-bar-animated" 
-                    style="background-color: {color2}; padding: calc(1rem + calc(var(--bs-border-width)* 2));border-top-left-radius: 0;border-bottom-left-radius: 0;" 
-                    bind:value={color2} placeholder="Шкала цвета" name="color2">
-                </div>
-                <sup class="text-secondary"><i>Данный цвет будет слева у карточки</i></sup>
-            </div>
-            {#if name.length <= 25 && congratulation.length <= 1024 && congratulation && name}
-            <button type="submit" class="btn btn-outline-success mb-3">Отправить</button>
-            {:else}
-            <button type="submit" class="btn btn-outline-success mb-3" disabled>Отправить</button>
-            {/if}
-            
-        </form>
-        <Card {name} {color} {color2} congrats={congratulation} />
+
+<div class="container-main">
+    {#if !yp.none}<h1 class="text-center mt-5">Ваш пост:</h1>
+    <div class="d-flex justify-content-center">
+        <Card name={yp.name} color={yp.color1} color2={yp.color2} congrats={yp.congrats} date={yp.date}> </Card>
+    </div>
+    <h1 class="text-center mt-3">А вот что написали остальные:</h1>
+    {:else}
+    <h1 class="text-center mt-5">Доска с поздравлениями Дани Мысливца</h1>
+    {/if}
+    <div class="d-flex w-100 flex-wrap justify-content-evenly align-items-center">
+        {#each posts as post}
+        <Card name={post.name} color={post.color1} color2={post.color2} congrats={post.congrats} date={post.date}> </Card>
+        {/each}
     </div>
 </div>
 
 <style>
-    .main-box {
-        min-width: 450px;
-        margin-right: auto;
-        margin-left: auto;
-    }
-    .container {
-        max-width: 800px;
+    .container-main {
+        margin-right: 60px;
+        margin-left: 60px;
     }
 </style>
